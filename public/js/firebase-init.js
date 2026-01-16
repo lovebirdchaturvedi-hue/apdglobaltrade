31bccd1c01818442f0d74513695e5f2428abcf19
Ashish Chaturvedi <lovebird.chaturvedi@gmail.com>
2026-01-16 20:58:37 +0530

    Add files via upload

---
+++ public/js/firebase-init.js
@@ -0,0 +1,18 @@
+<script type="module">
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "apd-globaltrade-prod.firebaseapp.com",
+  projectId: "apd-globaltrade-prod",
+  storageBucket: "apd-globaltrade-prod.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+</script>
